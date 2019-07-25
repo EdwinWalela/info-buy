c61f13d6d9a89773768eb23646ef4ce2804bdf34
edwinwalela <edwinwalela@gmail.com>
2019-07-25 16:12:26 +0300

    get item delivery date

--- crawler.js
+++ crawler.js
@@ -1,9 +1,13 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const Axios = require("axios");
 
-// URL to scrap
-const JUMIA_URL = "https://www.jumia.co.ke/catalog/?q=samsung+charger";
+const query = 'huawei';
 
+// URLs to scrap
+const JUMIA_URL = `https://www.jumia.co.ke/catalog/?q=${query}`;
+
+// Jumia Crawler
 let jumiaCrawler = (URL) => {
     // Send request
     request(URL,(err,res,body)=>{
@@ -14,13 +18,38 @@ let jumiaCrawler = (URL) => {
         let $ = cheerio.load(body);
     
         //Locate Products in DOM
-        $('.products>.sku.-gallery').each((i,el)=>{
-            let price = $(el).find('a.link>div.price-container.clearfix>.price-box.ri>span.price').children('span').next().attr('data-price');
-            let name = $(el).find('a.link>h2.title>span.name').text();
+        $('.sku.-gallery').each(async(i,el)=>{
+            // Get product details
+            let price = $(el).find('span.price').children('span').next().attr('data-price') || 'N/A';
+            let name = $(el).find('span.name').text();
+            let url = $(el).find('a.link').attr('href');
+            let date;
+
+            // Get item's delivery date
+            if(url){
+                try{
+                    delivery = await Axios.get(url);
+                    let $ = cheerio.load(delivery.data);
+                    $('section.sku-detail').each((i,el)=>{
+                        date = $(el).find('span.-description').text();
+                    })
+                }catch(err){
+                    console.log(err);
+                }
+            }
             
-            console.log('ksh.'+price+' - '+name);
+            let item = {
+                name,
+                price,
+                url,
+                date
+            }
+            if(item.name !== ''){
+                console.log(item);
+            }
         })
     })
 }
-// crwal websites
+
+// crawl websites
 jumiaCrawler(JUMIA_URL);
