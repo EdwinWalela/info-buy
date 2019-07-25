b6d467252e9ca0f4469207daa23652ddd180893b
edwinwalela <edwinwalela@gmail.com>
2019-07-25 19:41:47 +0300

    jiji crawler

--- crawler.js
+++ crawler.js
@@ -2,7 +2,7 @@ const request = require("request");
 const cheerio = require("cheerio");
 const Axios = require("axios");
 
-const query = 'huawei';
+const query = 'huawei+y7';
 
 // URLs to scrap
 const JUMIA_URL = `https://www.jumia.co.ke/catalog/?q=${query}`;
@@ -98,7 +98,38 @@ const kilimalCrawler = (URL) => {
 }
 
 const jijiCrawler = (URL) => {
+ // Send request
+    request(URL,(err,res,body)=>{
+        if(err){
+            console.log('error\n',err);
+        }
+        // Parse DOM
+        let $ = cheerio.load(body);
 
+        //Locate Products in DOM
+        $('div.h-flex-1-1-100.h-ph-0>div.b-list-advert__item.js-handle-click-ctr.js-advert-link').each((i,el)=>{
+            // Get product details
+            let name = $(el).find('a.qa-advert-title.js-advert-link').text().trim();
+            let price = $(el).find('div.b-list-advert__item-price>span').text().trim();
+            price = Number(price.replace("KSh","").trim().replace(",",""));
+            let url = $(el).find('a.qa-advert-title.js-advert-link').attr('href');
+            let condition =  $(el).find('small.b-list-advert__item-attr').text();
+            if(condition.includes("New") || name.includes('New')){
+                condition = 'new'
+            }else{
+                condition = 'used'
+            }
+
+            let item = {
+                name,
+                price,
+                url,
+                condition
+            }
+
+            console.log(item)
+        })
+    })
 }
 
 const pigiameCrawler = (URL) => {
