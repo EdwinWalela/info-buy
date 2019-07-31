bd37a98f10d713cec5649d835d6f2c289b976d4d
edwinwalela <edwinwalela@gmail.com>
2019-07-31 19:09:48 +0300

    homepage links

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use('/api/v1',spiderRoutes);
 
 app.get('/',(req,res)=>{
-    res.send("<a href='https://documenter.getpostman.com/view/4654837/SVYkw1vv?version=latest'>See Documentation</a>")
+    let data = "<a href='https://documenter.getpostman.com/view/4654837/SVYkw1vv?version=latest'>See Documentation</a>"
+    data+="<br/><br/><a target='_blank' href='http://infobuyke.surge.sh'>Demo</a>"
+    res.send(data);
 })
 
 app.listen(PORT,()=>{
